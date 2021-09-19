0cbc14b1df9e6cb3b2ccc568da6f5ed708b2e984
XIO <69510098+xio810@users.noreply.github.com>
2021-09-20 04:20:38 +0900

    react_prac4

---
+++ react_prac4.js
@@ -0,0 +1,33 @@
+import React, { useState } from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+const App = () => {
+  const [recordedNum, setRecordedNum] = useState(null);
+  const [num, setNum] = useState(0);
+
+  return (
+    <>
+      <div>숫자 : {num}</div>
+      {recordedNum == null ? (
+        <div>기록없음</div>
+      ) : (
+        <div>기록된 숫자 : {recordedNum}</div>
+      )}
+      <div>
+//증가
+        <button onClick={() => setNum(num + 1)}>증가</button>
+//기록
+        <button
+          onClick={() => {
+            setRecordedNum(num);
+            setNum(0);
+          }}
+        >
+          기록
+        </button>
+      </div>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
