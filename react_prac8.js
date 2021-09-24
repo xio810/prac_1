4ba5a6250a939015b0993feae7ce024220242f83
XIO <69510098+xio810@users.noreply.github.com>
2021-09-24 19:24:58 +0900

    react_prac8

---
+++ react_prac8.js
@@ -0,0 +1,46 @@
+import React, { useState } from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+const App = () => {
+  const [recordedNums, setRecordedNums] = useState([]);
+  const [num, setNum] = useState(0);
+
+  const saveRecord = () => {
+    if (num == 0) return;
+
+    setNum(0);
+    // 아래 2줄은 안됩니다.(불변성 위배)
+    /*
+    recordedNums.push(num);
+    setRecordedNums(recordedNums);
+    */
+    setRecordedNums([...recordedNums, num]);
+  };
+
+  let recordedList = <div>기록없음</div>;
+
+  if (recordedNums.length > 0)
+    recordedList = (
+      <div>
+        <h1>기록</h1>
+        <ul>
+          {recordedNums.map((num, index) => (
+            <li key={index}>{num}</li>
+          ))}
+        </ul>
+      </div>
+    );
+
+  return (
+    <>
+      <div>숫자 : {num}</div>
+      <div>
+        <button onClick={() => setNum(num + 1)}>증가</button>
+        <button onClick={saveRecord}>기록</button>
+      </div>
+      {recordedList}
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
