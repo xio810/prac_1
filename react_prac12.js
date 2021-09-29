f8d9b9068368b0573ae6326d8765ac22b8f80bc5
XIO <69510098+xio810@users.noreply.github.com>
2021-09-29 21:49:22 +0900

    react_prac12

---
+++ react_prac12.js
@@ -0,0 +1,17 @@
+console.clear();
+
+const App = () => {
+  let num = 0;
+  // 3번 증가
+  num++;
+  num++;
+  num++;
+
+  // 2번 감소
+  num--;
+  num--;
+
+  console.log("num : " + num); // 1
+}
+
+App();
